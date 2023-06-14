516b229a31747ca44d2dca2f7a8099827cbea821
Yashwant Sahu <87006594+yashwant4756@users.noreply.github.com>
2023-06-14 21:53:25 +0530

    Create string.sv

---
+++ string.sv
@@ -0,0 +1,19 @@
+module str;
+  string A;
+  string B;
+  string C;
+  initial
+    begin
+      A="ABCD";
+      B="TECHNOLOGIES";
+      C="tests";
+      $display("%d",A.len());
+      $display("%s",A.getc(4));
+      $display("%s",A.tolower());
+      $display("%s",C.toupper());
+      $display("%s",B.compare(A));
+      $display("%s",C.icompare(B));
+      $display("%s",C.substr());
+    end
+endmodule
+      
