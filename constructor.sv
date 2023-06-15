416cbb3941c6ecea3032edd8724446c622d40f21
Yashwant Sahu <87006594+yashwant4756@users.noreply.github.com>
2023-06-15 17:45:32 +0530

    Create constructor.sv

---
+++ constructor.sv
@@ -0,0 +1,20 @@
+
+class packet_c;
+	bit [31:0] src, dst;
+function new(int src, int dst = 10);
+	this.src = src;
+	dst = dst;
+endfunction
+  
+  function void display();
+    $display("src = %0d dst = %0d",src,dst);
+  endfunction
+endclass
+
+module way_2;
+packet_c pkt_h;
+  initial begin pkt_h = new(45,23);
+  pkt_h.display();
+  
+end
+endmodule
