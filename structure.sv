007239d57b04e426d813ccbaff7279b096eff963
Yashwant Sahu <87006594+yashwant4756@users.noreply.github.com>
2023-06-16 07:15:03 +0530

    Update structure.sv

--- structure.sv
+++ structure.sv
@@ -1,8 +1,9 @@
 module stru;
   struct{int x; byte y; bit [7:0] z}my_struct;
   my_struct.x=123;
-  my_struct.x=8'd27;
+  my_struct.z=8'd27;
   $display("The value of x is %d",my_struct.x);
+  $display("The value of z is %d",my_struct.z);
   
 endmodule
   
