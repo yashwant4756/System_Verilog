0472477c4c20782142f011b4e284efe1cd3f2906
Yashwant Sahu <87006594+yashwant4756@users.noreply.github.com>
2023-06-26 14:26:16 +0530

    Create Create print_array_from_c_into_sv_.c

---
+++ Create print_array_from_c_into_sv_.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<svdpi.h>
+
+ void arr(){
+   int i;
+  int array[5]={0,1,2,3,4};
+  printf("The values of array \n");
+  for( i=0;i<5;i++){
+    printf("array[%d]=%d \n",i,array[i]);
+  }
+}
