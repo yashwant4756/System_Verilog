6b52c92d3df5ca9055a7899ed0dc7adf8ba0b69d
Yashwant Sahu <87006594+yashwant4756@users.noreply.github.com>
2023-06-26 14:21:33 +0530

    Update dpi_c++file.c

--- dpi_c++file.c
+++ dpi_c++file.c
@@ -5,6 +5,10 @@ using namespace std;
 extern "C" void helloFromSV(svBit);
 
 extern "C" int helloFromCpp(svLogic a){
+  // 0 is 0
+  // 1 is 1
+  // 2 is z
+  // 3 is x
 int a_int = a;
   cout<<"(C++) a is "<<a_int<<endl;
   cout<<"Scpoe is "<<svGetNameFromScope(svGetScope())<<endl;
